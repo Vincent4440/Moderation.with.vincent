7d8e156d73e39ae918a5eb43e22d7e0e2aa55b99
Aditya Singh <88191487+Vincent4440@users.noreply.github.com>
2021-10-06 10:10:11 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-exports.Prefix = `'`;
-exports.Color = `RANDOM`;
-
-//Put the Token in Client secrets LOCK Button
-//Note- this bot was been maded by VINCENT 444#8079
-//If you need any help join my server https://discord.gg/WNSyk4pcMf
