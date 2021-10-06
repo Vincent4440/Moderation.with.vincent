6848d2fe896db6387eb6c1abf5a1eff85c485d57
Aditya Singh <88191487+Vincent4440@users.noreply.github.com>
2021-10-06 07:36:04 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+exports.Prefix = `'`;
+exports.Color = `RANDOM`;
+
+//Put the Token in Client secrets LOCK Button
+//Note- this bot was been maded by VINCENT 444#8079
+//If you need any help join my server https://discord.gg/WNSyk4pcMf
